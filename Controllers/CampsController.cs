79a8290e9e1f65daa9e4299cc410af4678dcddf3
Skander Chouba <skander.chouba@gmail.com>
2020-05-20 16:03:34 +0100

    Get Request entities from database

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreApiFundamentals.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database failure.");
+            }
+
+        }
+    }
+}
