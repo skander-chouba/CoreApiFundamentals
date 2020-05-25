46e2ecc763a25e2062f23a4b2432a1fa4a8f5d7a
Skander Chouba <skander.chouba@gmail.com>
2020-05-25 02:49:39 +0100

    Add functional API

---
+++ Controllers/OperationsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreApiFundamentals.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public OperationsController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpOptions("ReloadConfig")]
+        public  IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+    }
+}
